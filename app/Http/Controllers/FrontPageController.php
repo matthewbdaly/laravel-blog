bebdd3aa96003b6373f2d54765a3a67b36435023
Matthew Daly <matthewbdaly@gmail.com>
2017-12-01 19:32:16 +0000

    Home page in place

---
+++ app/Http/Controllers/FrontPageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace LaravelBlog\Http\Controllers;
+
+use Illuminate\Http\Request;
+use LaravelBlog\Post;
+
+class FrontPageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::orderBy('pub_date', 'desc')->limit(5)->get();
+        return view('home', [
+            'posts' => $posts
+        ]);
+    }
+}
