ba4fcd673800add70fc8d76a3d0edf718bf7a96a
Matthew Daly <matthewbdaly@gmail.com>
2017-12-24 15:59:27 +0000

    Added post repository

---
+++ app/Eloquent/Repositories/Post.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaravelBlog\Eloquent\Repositories;
+
+use Matthewbdaly\LaravelRepositories\Repositories\Base;
+use LaravelBlog\Contracts\Repositories\Post as PostContract;
+
+class Post extends Base implements PostContract
+{
+}
