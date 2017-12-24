84a5a5a24198f6f4914f779398a5f1affd7ebe95
Matthew Daly <matthewbdaly@gmail.com>
2017-12-24 15:57:45 +0000

    Added decorator

---
+++ app/Eloquent/Repositories/Decorators/Post.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaravelBlog\Eloquent\Repositories\Decorators;
+
+use Matthewbdaly\LaravelRepositories\Repositories\Decorators\BaseDecorator;
+use LaravelBlog\Contracts\Repositories\Post as PostContract;
+
+class Post extends BaseDecorator implements PostContract
+{
+}
