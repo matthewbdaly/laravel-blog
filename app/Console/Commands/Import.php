e69a0b1c864a8bac56bbc05f37e527688ccad02e
Matthew Daly <matthewbdaly@gmail.com>
2017-12-31 19:51:35 +0000

    Can now import pages as well as posts

--- app/Console/Commands/Import.php
+++ app/Console/Commands/Import.php
@@ -5,9 +5,11 @@ namespace LaravelBlog\Console\Commands;
 use Illuminate\Console\Command;
 use Mni\FrontYAML\Parser;
 use Storage;
-use LaravelBlog\Post;
-use LaravelBlog\User;
+use LaravelBlog\Eloquent\Models\Post;
+use LaravelBlog\Eloquent\Models\User;
+use Matthewbdaly\LaravelFlatpages\Eloquent\Models\Flatpage;
 use Carbon\Carbon;
+use DB;
 
 class Import extends Command
 {
@@ -43,15 +45,17 @@ class Import extends Command
      */
     public function handle()
     {
+        DB::statement('TRUNCATE TABLE posts');
+        DB::statement('TRUNCATE TABLE flatpages');
         $user = User::first();
-        $files = Storage::files('import');
-        foreach ($files as $file) {
-            $document = $this->parser->parse(Storage::get($file));
+        $posts = Storage::files('import/posts');
+        foreach ($posts as $post) {
+            $document = $this->parser->parse(Storage::get($post));
             $content = $document->getContent();
             $yaml = $document->getYAML();
             $pubDate = Carbon::parse($yaml['date']);
             $urlDate = preg_replace('/(\d{4})-(\d{2})-(\d{2})/', '$1/$2/$3/', $pubDate->toDateString());
-            $yaml['slug'] = "/" . $urlDate . preg_replace('/(\d{4})-(\d{2})-(\d{2})-/', '', explode(".", explode("/", $file)[1])[0]) . "/";
+            $yaml['slug'] = "/" . $urlDate . preg_replace('/(\d{4})-(\d{2})-(\d{2})-/', '', explode(".", explode("/", $post)[2])[0]) . "/";
             Post::create([
                 'text'=> $content,
                 'title' => $yaml['title'],
@@ -59,6 +63,19 @@ class Import extends Command
                 'pub_date' => $pubDate,
                 'author_id' => $user->id,
             ]);
+        }
+        $pages = Storage::files('import/pages');
+        foreach ($pages as $page) {
+            $document = $this->parser->parse(Storage::get($page));
+            $content = $document->getContent();
+            $yaml = $document->getYAML();
+            $yaml['slug'] = "/".explode(".", explode("/", $page)[2])[0]."/";
+            Flatpage::create([
+                'content'=> $content,
+                'title' => $yaml['title'],
+                'slug' => $yaml['slug'],
+            ]);
+
         }
     }
 }
