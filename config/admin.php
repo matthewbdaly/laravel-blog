1a4e00ab4e121e042f5afedd0b382900cfff1937
Matthew Daly <matthewbdaly@gmail.com>
2018-01-07 22:02:37 +0000

    Added config file for admin
    The idea is that models to be added to the admin should be set in the
    config.

---
+++ config/admin.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Registered models
+    |--------------------------------------------------------------------------
+    |
+    | Any models that should be shown in the admin must be registered here
+    |
+    */
+
+    'users' => 'LaravelBlog\Eloquent\Models\User',
+    'posts' => 'LaravelBlog\Eloquent\Models\Post',
+
+];
