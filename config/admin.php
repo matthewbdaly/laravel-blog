fabacd3f4d682a76dddb2b39ed9b0131ca83a31c
Matthew Daly <matthewbdaly@gmail.com>
2018-01-08 12:07:26 +0000

    Registered categories in admin

--- config/admin.php
+++ config/admin.php
@@ -14,6 +14,7 @@ return [
 
         'users' => 'LaravelBlog\Eloquent\Models\User',
         'posts' => 'LaravelBlog\Eloquent\Models\Post',
+        'categories' => 'LaravelBlog\Eloquent\Models\Category',
 
     ]
 ];
