cedbcc2bcb60ffa2d06c604ae94c821afc32b5f4
Matthew Daly <matthewbdaly@gmail.com>
2018-01-09 21:09:39 +0000

    Added comments, flags and flatpages to the admin

--- config/admin.php
+++ config/admin.php
@@ -16,6 +16,9 @@ return [
         'posts' => 'Matthewbdaly\LaravelBlog\Eloquent\Models\Post',
         'categories' => 'Matthewbdaly\LaravelBlog\Eloquent\Models\Category',
         'tags' => 'Matthewbdaly\LaravelBlog\Eloquent\Models\Tag',
+        'flatpages' => 'Matthewbdaly\LaravelFlatpages\Eloquent\Models\Flatpage',
+        'comments' => 'Matthewbdaly\LaravelComments\Eloquent\Models\Comment',
+        'flag' => 'Matthewbdaly\LaravelComments\Eloquent\Models\Comment\Flag',
 
     ]
 ];
