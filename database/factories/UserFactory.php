4fec9aa922a842663830ef9cb3443c730bc2a8bf
Matthew Daly <matthewbdaly@gmail.com>
2018-03-25 18:26:20 +0100

    Added failing test for the admin

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,7 +13,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(Matthewbdaly\LaravelBlog\User::class, function (Faker $faker) {
+$factory->define(Matthewbdaly\LaravelBlog\Eloquent\Models\User::class, function (Faker $faker) {
     static $password;
 
     return [
